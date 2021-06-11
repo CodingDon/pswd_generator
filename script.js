f9edffed04005afa2d0aa5a2935a196ceae28f0d
sheldon fletcher <shelcodingdon@gmail.com>
2021-06-11 16:18:51 -0400

    ucreated function and ifs

--- script.js
+++ script.js
@@ -1,4 +1,101 @@
 // Assignment Code
+var lowerChar = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+var upperChar = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+var numChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialChar = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+function pwPrompts() {
+// variable to take in a length as a number 
+  var pwLength = parseInt(prompt("How long do you want your password to be?"));
+
+  if(pwLength < 8) {
+    alert("Password needs to be 8 or more characters")
+  }
+  if(pwLength >128) {
+    alert("Password needs to be less than 128 characters")
+  }
+}
+
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
